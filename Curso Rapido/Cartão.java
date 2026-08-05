1aaa7c58d99f8fb58038e26b2dfa6461c4d016fc
Vinicius Meneses <138729617+ViniciuusMeneses@users.noreply.github.com>
2026-08-05 12:08:49 -0300

    Add Main class with introductory print statements

---
+++ Curso Rapido/Cartão.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("============================");
+        System.out.println();
+        System.out.println("        VINÍCIUS");
+        System.out.println("   Estudante de Java");
+        System.out.println("----------------------------");
+        System.out.println("  GitHub: ViniciuusMeneses");
+        System.out.println("  Pará, Brasil");
+        System.out.println();
+        System.out.println("============================");
+
+    }
+}
